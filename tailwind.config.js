4b0585e40dfedb97b493e747da98eae842f319ea
ZAKI TRI PAMUNGKAS <zakitripamungkas03@gmail.com>
2024-05-27 05:50:47 +0800

    update menambahkan section dan navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+        bckcolour: "#bfdbfe",
+      },
+    },
   },
   plugins: [],
 };
